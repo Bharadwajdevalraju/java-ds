7e4eb926c4860777234b0e69fd45cd8993694d8f
Bharadwaj Devalaraju <51433505+Bharadwajdevalraju@users.noreply.github.com>
2025-07-22 16:13:01 +0530

    new changes
    * fix(tree): change value's visibility
    
    * feat(version): change version
    
    * refactor(stack): simplify stack implementation and remove size limit
    
    * feat(linkedlist): implement addLast, addFirst, and reverse methods in ListNode
    
    * refactor(tree): remove size limit from stack in dfs method
    
    * feat(heap): implement Heap class with add, poll, and peek methods
    
    * chore(version): update version from 1.0.1 to 1.0.2

--- src/main/java/org/bd/linkedlist/ListNode.java
+++ src/main/java/org/bd/linkedlist/ListNode.java
@@ -6,4 +6,30 @@ public class ListNode<T> {
     public ListNode(T value) {
         this.value = value;
     }
+
+
+    public void addLast(T element) {
+        var head = this;
+        while (head.next != null) {
+            head=head.next;
+        }
+        head.next = new ListNode<>(element);
+    }
+
+    public void addFirst(T element) {
+        var cur = new ListNode<T>(element);
+        cur.next = this;
+    }
+
+    public ListNode<T> reverse() {
+        ListNode<T> prev = null;
+        var head = this;
+        while (head != null) {
+            var next = head.next;
+            head.next = prev;
+            prev= head;
+            head= next;
+        }
+        return prev;
+    }
 }
